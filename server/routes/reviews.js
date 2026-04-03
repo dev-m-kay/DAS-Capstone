5c48dff7aa46493e94a10a93728367803788dfc2
Sikon Toure <sikontoure@unomaha.edu>
2026-04-02 19:28:42 -0500

    adding review routes

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
+const { authenticate } = require('../middleware/auth');
+const { authorize } = require('../middleware/roles');
+const reviewController = require('../controllers/reviewController')
 
-// TODO: Add review routes (GET /mine, GET /:submissionId, POST /:submissionId)
+router.get('/mine',              authenticate, reviewController.getMyReviews);
+router.get('/:submissionId',     authenticate, reviewController.getForSubmission);
+router.post('/:submissionId',    authenticate, authorize('reviewer', 'editor'), reviewController.create);
+router.put('/:submissionId/:id', authenticate, authorize('reviewer', 'editor'), reviewController.update);
 
 module.exports = router;
